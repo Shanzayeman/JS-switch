6053e2fbfa4d6ec3905efbc3f4f961bb87289831
ShanzayEman <shanzayeman0@gmail.com>
2024-10-26 23:08:25 +0500

    Add files via upload

---
+++ 26oct.js
@@ -0,0 +1,36 @@
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+document.getElementById("demo1").innerHTML = "Today is " + day;
+let text;
+switch (new Date().getDay()) {
+  default:
+    text = "Looking forward to the Weekend";
+    break;
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+}
+document.getElementById("demo2").innerHTML = text;
